e9204d9fad5caab188e10c4e65c0bd7e434f9842
Alex Kruckenberg <mouldy.k@gmail.com>
2025-12-30 23:02:09 +0000

    more fiddling with wonder xml request

--- app/api/get-wonder-data/route.tsx
+++ app/api/get-wonder-data/route.tsx
@@ -4,14 +4,19 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { xml } = body;
 
+  console.log(
+    JSON.stringify({
+      request_xml: toWonderXML(xml),
+      accept_datause_restrictions: "true",
+    })
+  );
+
   return await fetch("https://wonder.cdc.gov/controller/datarequest/D149", {
     method: "POST",
     body: JSON.stringify({
       request_xml: toWonderXML(xml),
+      accept_datause_restrictions: "true",
     }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
   });
 
   // return new Response(JSON.stringify(xml), {
--- app/utils.tsx
+++ app/utils.tsx
@@ -1,39 +1,9 @@
-import { create, fragment } from "xmlbuilder2";
-import { XMLBuilder } from "xmlbuilder2/lib/interfaces";
+import { create } from "xmlbuilder2";
 
-const ACCEPT_DATAUSE_RESTRICTIONS_FRAGMENT =
-  "<parameter><name>accept_datause_restrictions</name><value>true</value></parameter>";
+import { DEFAULT_NATALITY_VARIABLES } from "./constants";
 
 export function toWonderXML(obj: object): string {
-  const fragments: XMLBuilder[] = [];
+  const doc = create().ele(DEFAULT_NATALITY_VARIABLES);
 
-  for (const [key, value] of Object.entries(obj)) {
-    fragments.push(
-      fragment()
-        .ele("parameter")
-        .ele("name")
-        .txt(key)
-        .up()
-        .ele("value")
-        .txt(value)
-        .up()
-        .up()
-    );
-  }
-
-  for (const frag of fragments) {
-    console.log(frag.end({ prettyPrint: true }));
-  }
-
-  const doc = create()
-    .ele("request-parameters")
-    .ele(ACCEPT_DATAUSE_RESTRICTIONS_FRAGMENT);
-
-  for (const frag of fragments) {
-    doc.up().import(frag);
-  }
-
-  console.log(doc.end({ prettyPrint: true }));
-
-  return doc.end();
+  return doc.end({ headless: true });
 }
