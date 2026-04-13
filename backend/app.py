ece9906e5735c0c35db25b38e280868bd422a31c
shroommu <alexa.kruckenberg@gmail.com>
2026-04-13 17:34:24 -0600

    scaffold vbac-predict

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pickle as pkl
 
 app = Flask(__name__)
 
@@ -6,3 +7,12 @@ app = Flask(__name__)
 @app.route("/api/health")
 def health():
     return {"status": "healthy"}
+
+
+@app.route("/api/predict-vbac")
+def predict_vbac(parameters={}):
+    model = pkl.load(open("models/vbac/rfc_model.pkl", "rb"))
+    return {
+        "parameters": parameters,
+        "vbac_prediction": "This is a placeholder prediction.",
+    }
