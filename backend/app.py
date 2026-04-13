4946bb484bed0094d4af659e6ca52c3fe586919e
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-04-13 09:26:26 -0600

    lighten up backend folder

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/health")
+def health():
+    return Flask.make_response({"status": "healthy"}, 200)
