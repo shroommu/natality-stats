ae4f74d3e2bcf11ce3eec601aa34ec89b623c46d
Alexa Kruckenberg <mouldy.k@gmail.com>
2026-04-13 10:05:17 -0600

    simplify health

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/api/health")
 def health():
-    return Flask.make_response({"status": "healthy"}, 200)
+    return {"status": "healthy"}
