6babd9500986309f0e7ed25be26e4c92312fecf2
shroommu <alexa.kruckenberg@gmail.com>
2026-05-15 09:46:02 -0600

    add new charts based on new model findings

---
+++ frontend/charts/vbac/FetalPresentationAtDeliveryCrossTabHeatmap.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useMemo } from "react";
+
+import ResponsiveHeatmap from "@/charts/ResponsiveHeatmap";
+import { ChartDataBoundary } from "@/components/ChartDataBoundary";
+import { useChartJsonCrossTab } from "@/hooks/useChartJson";
+
+import {
+  VBAC_OUTCOME_COLUMN_KEYS,
+  VBAC_OUTCOME_COLUMN_LABELS,
+  formatVbacCrossTabAnnotation,
+  formatVbacCrossTabTooltipBody,
+} from "./vbacCrossTabFormatters";
+import { VBAC_JSON_DATA_YEAR } from "./vbacDataYear";
+
+const DATA_FILE = "vbac/fetal_presentation_at_delivery_cross_tab.json";
+
+export default function FetalPresentationAtDeliveryCrossTabHeatmap() {
+  const { data, loading, error } = useChartJsonCrossTab(DATA_FILE, {
+    dataYear: VBAC_JSON_DATA_YEAR,
+  });
+
+  const rowKeysInOrder = useMemo(() => {
+    if (!data) return [];
+    const col = data[VBAC_OUTCOME_COLUMN_KEYS[0]];
+    return col ? Object.keys(col) : [];
+  }, [data]);
+
+  return (
+    <ChartDataBoundary loading={loading} error={error}>
+      {data && rowKeysInOrder.length > 0 && (
+        <ResponsiveHeatmap
+          title="Fetal Presentation at Delivery and Successful VBAC"
+          columnMajor={data}
+          columnKeysInOrder={[...VBAC_OUTCOME_COLUMN_KEYS]}
+          rowKeysInOrder={rowKeysInOrder}
+          columnLabels={VBAC_OUTCOME_COLUMN_LABELS}
+          rowLabels={{}}
+          xAxisLabel="Successful VBAC"
+          yAxisLabel="Fetal Presentation at Delivery"
+          formatAnnotation={formatVbacCrossTabAnnotation}
+          formatTooltipBody={formatVbacCrossTabTooltipBody}
+        />
+      )}
+    </ChartDataBoundary>
+  );
+}
